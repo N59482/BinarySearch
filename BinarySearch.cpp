1f285968e6d862abc2b38f934e2705c8bbaa284f
N59482 <iduginmail@ya.ru>
2021-12-09 14:07:51 +0300

    Алгоритм бинарного поиска

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#define SIZE 10
 
 using namespace std;
 
-void myqs(int* a, int left, int right)
+/*
+* Алгоритм быстрой сортировки.
+*/
+void QuickSort(int* a, int left, int right)
     {
         int il = left;
         int ir = right;
@@ -18,45 +22,66 @@ void myqs(int* a, int left, int right)
                         ir--;
                     };
             };
-        if (il < right) myqs(a,il,right);
-        if (left < ir) myqs(a,left,ir);
+        if (il < right) QuickSort(a,il,right);
+        if (left < ir) QuickSort(a,left,ir);
     };
 
-void sort_fill(int *a, const int size)
+/*
+* Функция sort_fill принимает:
+* массив в виде укателя типа int,
+* размерность этого массива в виде константного знчения.
+* максимальное значения для элемента массива Max_val в виде константного значения.
+* Далее заполняет полученный массив случайными числами в диапазоне от 0 до Max_val и сортирует массив быстрой сортировкой описанной выше.
+*/
+void sort_fill(int *a, const int size, const int Max_val)
 	{
-		for(int i = 0; i < size+1; i++)
+		for(int i = 0; i < size; i++)
 			{
-				a[i] = rand() % 11;
+				a[i] = rand() % Max_val;
 			};
-		myqs(a,0,size);
+		QuickSort(a,0,size-1);
 	};
 
-int BS(int * a, int size, int x)
+int BinarySearch(int * a, int size, int x)
 	{
 		int left = 0;
 		int right = size-1;
 		while(left != right)
 			{ 
 				int mid = (right+left)/2;
-				cout<<"test4\n";
-				if(a[mid] > x) {right = mid;cout<<"test5\n";}
-					else if (a[mid] < x) {left = mid;cout<<"test6\n";}
-							else {return mid;cout<<"test7\n";};
-							cout<<"test8\n";
+				if(a[mid] > x) right = mid;
+					else if (a[mid] < x) left = mid;
+							else return mid;
 			};			
 		return -404;
 	};
 
+/*
+* Функция для вывода массива в консоль.
+* Принимает указатель на первый элемент массива типа int 
+* Функция использует логику указателей для быстроты, хотя возможно в данном случае это не так актуально.
+*/
+void show(int *a, const int size)
+	{
+		int * end = a + size;
+		for(a; a < end; a++)
+			cout<<*a<<" ";
+		cout<<endl;
+	}; 
+
 int main()
 	{
 		int a[10];
-		sort_fill(a,10);
-		for(auto & i : a)
-			cout<<i<<" ";
-		cout<<endl;
+		int index[] = {0,1,2,3,4,5,6,7,8,9};
+        
+		sort_fill(a,SIZE,10);
+        
+        show(index, SIZE);
+		show(a, SIZE);
+
 		int x = 0;
 		cout<<"What are u looking for?\n";
 		cin>>x;
-		cout<<"There is "<<BS(a,10,x);
+		cout<<"index is "<<BinarySearch(a,10,x);
 		return 0;
 	}; 
